8b67200c0545ada7aead98b97443c8e7f53838d9
Leandro Franco <llefranco@gmail.com>
2026-01-29 21:39:48 -0300

    algoritmo-imc

---
+++ algoritmo-imc.py
@@ -0,0 +1,11 @@
+imc = float(input("Digite seu IMC: "))
+if (imc<18.5):
+    print ("Sua classificação de IMC é Abaixo do peso")
+elif (imc<24.9):
+    print ("Sua classificação de IMC é Peso normal")
+elif (imc<29.9):
+    print ("Sua classificação de IMC é Sobrepeso")
+elif (imc<39.9):
+    print ("Sua classificação de IMC é Obesidade")
+else:
+    print ("Sua classificação de IMC é Obesidade Grave")
